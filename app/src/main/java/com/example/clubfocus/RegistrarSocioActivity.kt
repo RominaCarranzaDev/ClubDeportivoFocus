b58297fcbb5cc6882e0a3faee02bc5470fa883f4
Graciela Brayan <C943765@dorinka.com>
2026-09-22 14:37:42 -0300

    Creacion y validacion de la pantalla RegistrarSocio con formulario y navegacion completa

--- app/src/main/java/com/example/clubfocus/RegistrarSocioActivity.kt
+++ app/src/main/java/com/example/clubfocus/RegistrarSocioActivity.kt
@@ -1,6 +1,12 @@
 package com.example.clubfocus
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +17,39 @@ class RegistrarSocioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar_socio)
+
+        // 1. Boton Volver
+        val tvVolver = findViewById<TextView>(R.id.tvVolver)
+        tvVolver.setOnClickListener {
+            finish()
+        }
+
+        // 2. Vincular componentes del formulario:
+        val rbSocio = findViewById<RadioButton>(R.id.rbSocio)
+        val cbAptoFisico = findViewById<CheckBox>(R.id.cbAptoFisico)
+        val etNombre = findViewById<EditText>(R.id.etNombre)
+        val etApellido = findViewById<EditText>(R.id.etApellido)
+        val etDni = findViewById<EditText>(R.id.etDni)
+        val btnGuardarRegistro = findViewById<Button>(R.id.btnGuardarRegistro)
+
+        // 3. Accion Boton Guardar
+        btnGuardarRegistro.setOnClickListener {
+            val nombre = etNombre.text.toString().trim()
+            val apellido = etApellido.text.toString().trim()
+            val dni = etDni.text.toString().trim()
+
+            val presentoApto = cbAptoFisico.isChecked
+            val esSocio = rbSocio.isChecked
+
+            if (nombre.isEmpty() || apellido.isEmpty() || dni.isEmpty()) {
+                Toast.makeText(this, "Por favor, completa nombre, apellido y DNI", Toast.LENGTH_SHORT).show()
+            } else {
+                val tipoUsuario = if (esSocio) "Socio" else "No Socio"
+                val mensajeConfirmacion = "$tipoUsuario $nombre $apellido registrado correctamente"
+
+                Toast.makeText(this, mensajeConfirmacion, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
 }
